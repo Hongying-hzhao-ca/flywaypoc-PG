ef967dcf1dbb566e4313550eba74d65fc24f2bf0
winston-zhou-ca <148275396+winston-zhou-ca@users.noreply.github.com>
2023-10-27 08:45:38 -0400

    Update V2023.10.25.001__CreateRole_Grant.sql

--- migrations/V2023.10.25.001__CreateRole_Grant.sql
+++ migrations/V2023.10.25.001__CreateRole_Grant.sql
@@ -4,16 +4,16 @@ targetdb text;
 BEGIN
 targetdb := '${flyway:database}';
 IF (targetdb = 'wz_dev') THEN
-        create role dev_user1 login password 'password1';
-        grant select on myuser to dev_user1; 
+        create role dev_user2 login password 'password1';
+        grant select on myuser to dev_user2; 
 ELSIF (targetdb = 'wz_qa')  THEN
-        create role qa_user1 login password 'password1';
-        grant select on myuser to qa_user1;
+        create role qa_user2 login password 'password1';
+        grant select on myuser to qa_user2;
 ELSIF (targetdb = 'wz_uat')  THEN
-        create role qa_user1 login password 'password1';
-        grant select on myuser to uat_user1;
+        create role qa_user2 login password 'password1';
+        grant select on myuser to uat_user2;
 ELSIF (targetdb = 'wz_prod')  THEN
-        create role prod_user1 login password 'password1';
-        grant select on myuser to prod_user1;
+        create role prod_user2 login password 'password1';
+        grant select on myuser to prod_user2;
 END IF;
 END $$;
