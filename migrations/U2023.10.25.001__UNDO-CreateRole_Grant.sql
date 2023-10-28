24ea4e13c9c578f1357941fef9df8a44b273d013
winston-zhou-ca <148275396+winston-zhou-ca@users.noreply.github.com>
2023-10-27 20:14:57 -0400

    Update U2023.10.25.001__UNDO-CreateRole_Grant.sql

--- migrations/U2023.10.25.001__UNDO-CreateRole_Grant.sql
+++ migrations/U2023.10.25.001__UNDO-CreateRole_Grant.sql
@@ -4,13 +4,13 @@ targetdb text;
 BEGIN
 targetdb := '${flyway:database}';
 IF (targetdb = 'wz_dev') THEN
-        revoke select on myuser from dev_user2; 
-        drop user dev_user2;
+        revoke select on myuser from dev_user1; 
+        drop user dev_user1;
 ELSIF (targetdb = 'wz_qa')  THEN
-        revoke select on myuser from qa_user2; 
-        drop user qa_user2;
+        revoke select on myuser from qa_user1; 
+        drop user qa_user1;
 ELSIF (targetdb = 'wz_prod')  THEN
-        revoke select on myuser from prod_user2; 
-        drop user prod_user2;
+        revoke select on myuser from prod_user1; 
+        drop user prod_user1;
 END IF;
 END $$;
